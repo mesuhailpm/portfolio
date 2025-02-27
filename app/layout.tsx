4eff8ab27fa241eb494aa77cdcc2f3fcc225375b
suhailpm <tosuhailpm@gmail.com>
2025-02-27 13:36:16 +0530

    feat: Integrate ScrollToTop component and update layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.scss";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
+import ScrollToTop from "@components/helper/scrollToTop";
+
 
 export const metadata: Metadata = {
   title: "Suhail PM | Portfolio",
@@ -19,9 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={``}
       >
+        <ToastContainer />
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <Navbar />
         {children}
+        </main>
+        <ScrollToTop />
         <Footer />
       </body>
     </html>
