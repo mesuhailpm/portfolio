5d62f947448aff44eb16d3918916d075e050433d
suhailpm <tosuhailpm@gmail.com>
2025-02-27 12:55:35 +0530

    feat: Add ExperienceSection, GlowCard, and AnimationLottie components
    - Introduced ExperienceSection component to showcase professional experiences.
      - Included a section with a title "Experiences" and a list of work experiences.
      - Added an animation using Lottie for visual enhancement.
      - Used GlowCard component to display each experience item with details such as duration, title, and company.
      - Applied TailwindCSS for styling the component.
    
    - Introduced GlowCard component to create an interactive glowing card effect based on cursor proximity.
      - Implemented useEffect to handle the glow effect and update card styles dynamically.
      - Added configuration options for proximity, spread, blur, gap, vertical alignment, and opacity.
      - Applied TailwindCSS for styling the component.
    
    - Introduced AnimationLottie component to render Lottie animations.
      - Configured default options for looping, autoplay, and animation data.
      - Applied custom styles for the animation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@components/Hero";
 import AboutSection from "@components/AboutSection";
+import ExperienceSection from "@components/ExperienceSection";
 import EducationSection from "@components/EducationSection";
 
 export default function Home() {
   return (
         <Hero />
         <AboutSection />
+        <ExperienceSection />
         <EducationSection />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
