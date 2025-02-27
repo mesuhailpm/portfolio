323551b8078acad356e3bb3b1c3b5e979b60e9f2
suhailpm <tosuhailpm@gmail.com>
2025-02-27 12:36:05 +0530

    Update ESLint configuration to disable specific TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,17 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
