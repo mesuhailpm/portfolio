f2c3a488feb3e2019f7e2894511aba61b1393dbf
suhailpm <tosuhailpm@gmail.com>
2025-03-01 14:18:50 +0530

    some more icon related things generated by https://realfavicongenerator.net/

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+       <meta name="apple-mobile-web-app-title" content="Portfolio | Suhail PM" />
+      </head>
       <body
         className={``}
       >
